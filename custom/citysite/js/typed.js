1256888d4c95c75f6b33b7f10f98e584478f2354
jer bartels <jer@satch.cc>
2018-01-16 08:14:10 +0100

    TypeJS
    Add TypeIT

--- custom/citysite/js/typed.js
+++ custom/citysite/js/typed.js
@@ -3,7 +3,16 @@
     Drupal.behaviors.typed = {
         attach: function (context, settings) {
             $(document).ready(function(){
-
+                new TypeIt('.field--name-field-titelpagina-ondertitel', {
+                    speed: 45,
+                    startDelay: 1000
+                })
+                    .pause(2000)
+                    .options({speed: 300})
+                    .delete(11)
+                    .options({speed: 45})
+                    .pause(1000)
+                    .type('in het groen');
             });
         }
     };
